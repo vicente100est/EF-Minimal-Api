8ba1f53c360964bc437f9726e66de5a7bc14d3bf
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2022-11-03 17:44:33 -0600

    Add DBContext config and DataAnnotation

--- MinimalApi/Models/Category.cs
+++ MinimalApi/Models/Category.cs
@@ -1,8 +1,13 @@
-﻿namespace MinimalApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinimalApi.Models
 {
     public class Category
     {
+        [Key]
         public Guid CategoryId { get; set; }
+        [Required]
+        [MaxLength(150)]
         public string Name { get; set; }
         public string Description { get; set; }
 
